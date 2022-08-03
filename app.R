d39d37b744f731757beb4a8ba16e32a1f3555ca5
David Chan <dchan@waikato.ac.nz>
2022-08-04 10:33:28 +1200

    hi

--- app.R
+++ app.R
@@ -1,48 +1,90 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+library(shiny); library(ggplot2)
 
-library(shiny)
+president.df = read.csv("Sampling-Presidents-2022.csv")
 
-# Define UI for application that draws a histogram
+colnames(president.df)[3] = "Height (inches)"
+colnames(president.df)[4] = "Age (years)"
+
 ui <- fluidPage(
+  titlePanel("CSMAX101-22B Worksheet 1"),
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+  sidebarLayout(
+    ##
+    sidebarPanel(
+      sliderInput("nInput", "Sample size (n):", min = 1, max = 46, value = 5),
+      actionButton("singlePlay", "Sample!", class = "btn-success"),
+      hr(),
+      strong(HTML("Sample mean (x&#772;): ")), textOutput("xbar", inline = TRUE), br(),
+    ),
+  
+    ##
+    mainPanel(
+      tabsetPanel(type = "tabs", id = "viewport",
+        ##
+        tabPanel("Simple Random Sampler",
+          plotOutput("samplePlot", height = "300px", width = "500px"),
+          plotOutput("populationPlot", height = "300px", width = "500px"),
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+            
+        ##
+        tabPanel("Full Data Set",
+          dataTableOutput("data")
         )
-    )
+    ))
+  )
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
+  RAM = reactiveValues(sampleData.df = data.frame())
+  
+  ##
+  # sampleData.df <- observe({
+  #   president.df[sample(1:nrow(president.df), input$nInput), ]
+  #   }) %>%
+  #   bindEvent(input$singlePlay)
+  
+  # reactiveA = bindEvent(input$singlePlay, reactive({
+  #   RAM$sampleData.df = president.df[sample(1:nrow(president.df), input$nInput), ]
+  # }))
+  
+  observer = observe({
+    RAM$sampleData.df = president.df[sample(1:nrow(president.df), input$nInput), ]
+  }) |>
+    bindEvent(input$singlePlay)
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  ##
+  output$samplePlot = renderPlot({
+    req(nrow(RAM$sampleData.df) > 0)
+    ggplot(data = RAM$sampleData.df, aes(x = `Height (inches)`)) +
+      geom_histogram(aes(y = ..density..), binwidth = 1, fill = "tomato", col = "black") +
+      ggtitle("Sample Distribution of US Presidents' Heights") +
+      xlim(range(president.df[, 3])) +
+      ylab("Density") +
+      scale_y_continuous(labels = function(x) sprintf("%.2f", x)) +
+      theme_bw() +
+      theme(title = element_text(size = 14))
+  })
+  
+  ##
+  output$xbar = renderText({
+    req(nrow(RAM$sampleData.df) > 0)
+    mean(RAM$sampleData.df[, 3])
+  })
+  
+  ##
+  output$populationPlot = renderPlot({
+    ggplot(data = president.df, aes(x = `Height (inches)`)) +
+      geom_histogram(aes(y = ..density..), binwidth = 1, fill = "lightblue", col = "black") + 
+      ggtitle("Population Distribution of US Presidents' Heights") +
+      ylab("Density") +
+      scale_y_continuous(labels = function(x) sprintf("%.2f", x)) +
+      xlim(range(president.df[, 3])) +
+      theme_bw() +
+      theme(title = element_text(size = 14))
+  })
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+  ##
+  output$data = renderDataTable(president.df, options = list(pageLength = 10))
 }
 
 # Run the application 
