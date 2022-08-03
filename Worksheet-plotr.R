ec41a6fe705a628586251a28adc661a2778868fa
David Chan <dchan@waikato.ac.nz>
2022-08-03 14:05:54 +1200

    ?

--- Worksheet-plotr.R
+++ Worksheet-plotr.R
@@ -4,10 +4,12 @@ x11()
 
 form.data = data.frame(ids = 1:10, n.05 = rnorm(10, 5, 25), n.15 = rnorm(10, 5, 10))
 
-colnames(form.data)[2:3] = c("n.05", "n.15")
+colnames(form.data)[2:3] = c("Sample Size = 5", " Sample Size = 15")
 
 form.data.flip = form.data |>
   pivot_longer(cols = colnames(form.data)[2:3])
+
+form.data.flip$name = factor(form.data.flip$name, levels = c("Sample Size = 5", " Sample Size = 15"))
 
 ggplot(data = form.data.flip, aes(x = value)) +
   geom_histogram(bins = 15, fill = "lightblue", col = "black") + 
