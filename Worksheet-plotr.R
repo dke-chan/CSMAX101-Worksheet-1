fb85ca1097121b3f840a3828c233e28ad3d28656
David Chan <dchan@waikato.ac.nz>
2022-08-05 10:10:26 +1200

    Implemented manual random sampling if students don't do it..

--- Worksheet-plotr.R
+++ Worksheet-plotr.R
@@ -2,7 +2,15 @@ library(ggplot2); library(tidyr)
 
 x11()
 
-form.data = data.frame(ids = 1:10, n.05 = rnorm(10, 5, 25), n.15 = rnorm(10, 5, 10))
+first <- numeric(100) -> second
+
+for (i in 1:100) {
+  first[i] = mean(president.df[sample(1:46, 5), 3])
+  second[i] = mean(president.df[sample(1:46, 15), 3])
+}
+
+form.data = data.frame(ids = 1:100, n.05 = first, n.15 = second)
+# form.data = read.csv(file.choose())
 
 colnames(form.data)[2:3] = c("Sample Size = 5", " Sample Size = 15")
 
@@ -12,8 +20,9 @@ form.data.flip = form.data |>
 form.data.flip$name = factor(form.data.flip$name, levels = c("Sample Size = 5", " Sample Size = 15"))
 
 ggplot(data = form.data.flip, aes(x = value)) +
-  geom_histogram(bins = 15, fill = "lightblue", col = "black") + 
+  geom_histogram(aes(y=..ndensity..), bins = 15, fill = "lightblue", col = "black") + 
   facet_wrap(~ name) +
   theme_bw() +
+  geom_vline(xintercept = 70.83, col = "red", size = 2) +
   xlab("Sample Mean Height (in)") +
   ylab("Frequency")
